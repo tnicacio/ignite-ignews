6e795e9f926e1dc7f62a4814ca1d76b72a41d62a
tnicacio <nicacio.t92@gmail.com>
2021-04-01 23:23:26 -0300

    working with prismic cms

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,57 @@
-export default function Home() {
+import { GetStaticProps } from 'next';
+import Head from 'next/Head';
+import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
+
+import styles from './home.module.scss';
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
-   <h1>Hello World</h1>
+    <>
+      <Head>
+        <title>Home | Ignews</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about <br /> the <span>React</span> world.</h1>
+          <p>
+            Get access to all publications <br />
+            <span>for {product.amount} per month</span>
+          </p>
+          <SubscribeButton priceId = {product.priceId}/>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding"/>
+      </main>
+    </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1IYY4PIu6dj5dSO7SZosW9YM', {
+    expand: ['product']
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100), //it's saved in cents, so the division
+  };
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  }
 }
